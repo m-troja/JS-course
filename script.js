f3492d91e8bb2242d0bf3a1cd3b22bb806d324ab
m-troja <mtroja98@gmail.com>
2025-06-08 23:12:25 +0200

    JS objects

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-let js = 'amazing';
-if (js == 'amazing') alert('elo michal');
-40 + 8;
-console.log(40 + 8);
+﻿
+let mark = {
+    fullName: 'Mark Miller',
+    mass: '78',
+    height: '1.69',
+
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+}
+
+let john = {
+    fullName: 'John Smith',
+    mass: '92',
+    height: '1.95',
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+}
+mark.calcBMI();
+john.calcBMI();
+if (john.bmi > mark.bmi) {
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`);
+}
+else console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`);
